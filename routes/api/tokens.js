7c24f04564231ddb9def07a491a0b86e82f6fcbe
kangere <kangere721@gmail.com>
2019-06-25 23:49:13 -0400

    Added tokens.controller.js with empty method stubs

--- routes/api/tokens.js
+++ routes/api/tokens.js
@@ -1,58 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
+const tokensController = require('../../controllers/tokens.controller.js');
+
 // @route   GET api/users/{userId}/tokenRequest
 // @desc    Returns all user token requests
 // @access  Private
 router.get('/users/:userId/tokenRequest', 
-	(req, res) => {
-			let userId = req.params.userId
+	tokensController.getAllUserTokenRequests)
 
-			res.json({ msg: 'NOT IMPLEMENTED: token request for user ' + userId })
-		})
+
 
 // @route   Post api/users/{userId}/tokenRequest
 // @desc    Adds tokenRequest for user with is {userId}
 // @access  Private
 router.post('/users/:userId/tokenRequest', 
-	(req, res) => res.json({ msg: 'NOT IMPLEMENTED: token request Post' }))
+	tokensController.postUserTokenRequest)
 
 
 // @route   GET api/users/{userId}/tokenRequest/{tokenRequestId}
 // @desc    Returns tokenRequest with tokenRequestId for the user with UserId
 // @access  Private
 router.get('/users/:userId/tokenRequest/:tokenRequestId', 
-	(req, res) => {
-			let userId = req.params.userId
-			let tokenRequestId = req.params.tokenRequestId
-
-			res.json({ msg: 'NOT IMPLEMENTED: token request for user ' 
-				+ userId + ' and tokenRequest ' + tokenRequestId })
-		})
+	tokensController.getUserTokenRequest)
 
 
 // @route   GET api/users/{userId}/tokens
 // @desc    Returns all tokens for user with ID userId
 // @access  Private 
 router.get('/users/:userId/tokens', 
-	(req, res) => {
-			let userId = req.params.userId
-
-			res.json({ msg: 'NOT IMPLEMENTED: tokens  for user ' 
-				+ userId})
-		})
+	tokensController.getAllUserTokens)
 
 
 // @route   GET api/users/{userId}/tokens/{tokenId}
 // @desc    Returns all tokens for user with ID userId
 // @access  Private 
 router.get('/users/:userId/tokens/:tokenId', 
-	(req, res) => {
-			let userId = req.params.userId
-			let tokenId = req.params.tokenId
+	tokensController.getUserToken)
 
-			res.json({ msg: 'NOT IMPLEMENTED: tokens  for user ' 
-				+ userId + ' for token ' + tokenId})
-		})
+
 
 module.exports = router;
